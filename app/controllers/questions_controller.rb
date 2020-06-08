24f3e55a2517624b3858db72171b45f9fb579f49
Remi <remi.andrivet@hotmail.com>
2020-06-08 12:58:17 +0200

    Done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ans = ""
+    if params[:question] == "I am going to work"
+      @ans = "Great!"
+    elsif params[:question].include? "?"
+      @ans = "Silly question, get dressed and go to work!"
+    else
+      @ans = "I don't care, get dressed and go to work!"
+    end
+  end
+end
---
+++ test/controllers/remi_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RemiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
